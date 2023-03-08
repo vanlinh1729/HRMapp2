ea3dc74c1d0ef121e4868a97c4d95d057d9656c5
Admin <vanlinh1729@gmail.com>
2023-03-09 00:03:08 +0700

    menu created

--- src/HRMapp2.Web/Menus/HRMapp2MenuContributor.cs
+++ src/HRMapp2.Web/Menus/HRMapp2MenuContributor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using HRMapp2.Localization;
 using HRMapp2.MultiTenancy;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Identity.Web.Navigation;
 using Volo.Abp.SettingManagement.Web.Navigation;
 using Volo.Abp.TenantManagement.Web.Navigation;
@@ -27,39 +29,152 @@ public class HRMapp2MenuContributor : IMenuContributor
             0,
             new ApplicationMenuItem(
                 HRMapp2Menus.Home,
-                l["Menu:Home"],
+                l["Trang chủ"],
                 "~/",
                 icon: "fas fa-home",
                 order: 0
             )
-            
+
         );
-        
+
         context.Menu.Items.Insert(
             1,
             new ApplicationMenuItem(
                 "Departments",
-                l["Departments"],
+                l["Phòng ban"],
                 "/departments",
-                icon: "fas fa-home",
-                order: 0
+                icon: "fas fa-building"
+
             )
-            
+
         );
+
         context.Menu.Items.Insert(
-            1,
+            2,
             new ApplicationMenuItem(
                 "Employees",
-                l["Employees"],
+                l["Nhân viên"],
                 "/employees",
-                icon: "fas fa-home",
-                order: 0
+                icon: "fas fa-user-circle"
             )
-            
-        );
-        
 
-        if (MultiTenancyConsts.IsEnabled)
+        );
+
+        context.Menu.AddItem(
+            new ApplicationMenuItem("HRMapp.Recruitment", l["Tuyển dụng"],"", icon: "fas fa-bullhorn")
+                .AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Recruitment", 
+                    name: "Recruitment.Needs",
+                    displayName: l["Nhu cầu nhân sự"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Recruitment", 
+                    name: "Recruitment.Plan",
+                    displayName: l["Kế hoạch tuyển dụng"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Recruitment", 
+                    name: "Recruitment.Candidate",
+                    displayName: l["Ứng viên"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Recruitment", 
+                    name: "Recruitment.Enterview1",
+                    displayName: l["Phỏng vấn lần 1"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Recruitment", 
+                    name: "Recruitment.Enterview2",
+                    displayName: l["Phỏng vấn lần 2"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Recruitment", 
+                    name: "Recruitment.ProbationStaff",
+                    displayName: l["Nhân viên thử việc"],
+                    url: "~/")
+                ));
+
+        context.Menu.AddItem(
+            new ApplicationMenuItem("HRMapp.DayOffs", l["Xin nghỉ"],"", icon: "fas fa-file-alt")
+                .AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.DayOffs", 
+                    name: "DayOffs.AllLeaveApplication",
+                    displayName: l["Tất cả đơn xin nghỉ"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.DayOffs", 
+                    name: "DayOffs.DayOff",
+                    displayName: l["Xin nghỉ"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.DayOffs", 
+                    name: "DayOffs.DayOffCustom",
+                    displayName: l["Xin đi sớm về muộn"],
+                    url: "~/")
+                ).AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.DayOffs", 
+                    name: "DayOffs.PublicDayOff",
+                    displayName: l["Ngày nghỉ chung"],
+                    url: "~/")
+                ));
+        context.Menu.AddItem(
+            new ApplicationMenuItem("HRMapp.Timekeeping", l["Chấm công"],"", icon: "fas fa-business-time")
+                .AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Timekeeping", 
+                    name: "Timekeeping.Month",
+                    displayName: l["Chấm công theo tháng"],
+                    url: "~/")
+                ) .AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Timekeeping", 
+                    name: "Timekeeping.Day",
+                    displayName: l["Chấm công theo ngày"],
+                    url: "~/")
+                ) .AddItem(new ApplicationMenuItem(
+                    target: "_HRMapp.Timekeeping", 
+                    name: "Timekeeping.Config",
+                    displayName: l["Cấu hình chấm công"],
+                    url: "~/")
+                ));
+
+        context.Menu.Items.Insert(
+            7,
+            new ApplicationMenuItem(
+                "Payrolls",
+                l["Bảng lương"],
+                
+                "~/",
+                
+                icon: "fas fa-table"
+
+            )
+
+        );
+        context.Menu.Items.Insert(
+            8,
+            new ApplicationMenuItem(
+                "Contracts",
+                l["Hợp đồng"],
+                "~/",
+                icon: "fas fa-pen"
+
+            )
+
+        );
+        context.Menu.Items.Insert(
+            9,
+            new ApplicationMenuItem(
+                "Insurances",
+                l["Bảo hiểm"],
+                "~/",
+                icon: "fas fa-pen"
+            )
+
+        );
+
+       
+
+
+    if (MultiTenancyConsts.IsEnabled)
         {
             administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);
         }
--- src/HRMapp2.Web/Menus/HRMapp2Menus.cs
+++ src/HRMapp2.Web/Menus/HRMapp2Menus.cs
@@ -4,6 +4,15 @@ public class HRMapp2Menus
 {
     private const string Prefix = "HRMapp2";
     public const string Home = Prefix + ".Home";
+    public const string Department = Prefix + ".Department";
+    public const string Employee = Prefix + ".Employee";
+    public const string Recruitment = Prefix + ".Recruitment";
+    public const string DayOff = Prefix + ".DayOff";
+    public const string Timekeeping = Prefix + ".Timekeeping";
+    public const string Payroll = Prefix + ".Payroll";
+    public const string Contract = Prefix + ".Contract";
+    public const string Insurance = Prefix + ".Insurance";
+    
 
     //Add your menu items here...
 
